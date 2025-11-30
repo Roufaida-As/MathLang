0ae5f5df35a1711a95672b71cdd5baa80e8f9eee
Roufaida <mr_asbar@esi.dz>
2025-11-30 23:01:37 +0100

    merging the three parts of the lexer

--- tests/test3_conditions.ml
+++
@@ -1,8 +0,0 @@
-# Test 3: Structure SI
-SI x > 0 ALORS
-    AFFICHER("positif")
-SINON SI x < 0 ALORS
-    AFFICHER("negatif")
-SINON
-    AFFICHER("nul")
-FIN
--- tests/test5_commentaires.ml
+++
@@ -1,6 +0,0 @@
-# Test 5: Commentaires et opérateurs
-# Ceci est un commentaire
-SOIT x dans Z tel que x <- 42  # Commentaire en fin de ligne
-SI x >= 0 et x <= 100 ALORS
-    AFFICHER("valide")
-FIN
